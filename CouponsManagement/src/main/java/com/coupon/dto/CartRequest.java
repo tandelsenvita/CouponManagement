fb61358502e309be442fda6a03d32d83ac7afbe6
tandelsenvita <104340659+tandelsenvita@users.noreply.github.com>
2024-10-11 17:56:22 +0530

    Update CartRequest.java

--- CouponsManagement/src/main/java/com/coupon/dto/CartRequest.java
+++ CouponsManagement/src/main/java/com/coupon/dto/CartRequest.java
@@ -4,9 +4,7 @@ import lombok.Data;
 
 import java.util.List;
 @Data
-@RequiredArgsConstructor
 public class CartRequest {
-    @NonNull
     private List<CartItem> items;
     private Double totalPrice;
     private Double totalDiscount;
